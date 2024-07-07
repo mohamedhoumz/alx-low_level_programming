bd39cfb347ca61861efedf1e462963bc232f5154
Mohamed Houmz <mohammed.houmz@gmail.com>
2024-07-07 13:11:11 -0700

    fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-echo '#include <unistd.h>' > 101-quote.c && echo '#include <stdio.h>' >> 101-quote.c && echo '' >> 101-quote.c && echo 'int main(void){return dprintf(2,"and that piece of art is useful" - Dora Korpar, 201 Day  compile without so 202# 204#include. executed
+#include <unistd.h>
+
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
+}
+
